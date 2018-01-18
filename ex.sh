74f3287b06bd1831adf559b863fa296745dec45b
Phani Rithvij <phanirithvij2000@gmail.com>
2018-01-18 19:45:03 +0530

    bug cd

--- ex.sh
+++ ex.sh
@@ -1,16 +1,8 @@
 IFS=$'\n'
 echo '#start' > output.txt
-for i in $(cat .tmp.txt)
-do
-echo $i > ex2.sh
+cat .tmp.txt > ex2.sh
 chmod +x ex2.sh
-echo $i >> output.txt
 ./ex2.sh >> output.txt
-	if [[ "$(echo $i | sed '/^$/d;s/[[:blank:]]//g' | head -c 1)" != "#" ]]
-	then
-echo " " >> output.txt
-	fi
-done
 if [ -f gitauto.sh  ]
 then
 echo '#Git sucessful' >> output.txt
