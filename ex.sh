70d923f022727f25d6e1e2874549af1c80e911c8
phanirithvij <phanirithvij20003@gmail.com>
2018-01-18 19:46:19 +0530

    Thursday 03 2018 07:46:17 PM CR13065

--- ex.sh
+++ ex.sh
@@ -1,16 +1,9 @@
 IFS=$'\n'
 echo '#start' > output.txt
-for i in $(cat .tmp.txt)
-do
-echo $i > ex2.sh
+echo .tmp.txt > ex2.sh
 chmod +x ex2.sh
-echo $i >> output.txt
 ./ex2.sh >> output.txt
-	if [[ "$(echo $i | sed '/^$/d;s/[[:blank:]]//g' | head -c 1)" != "#" ]]
-	then
-echo " " >> output.txt
-	fi
-done
+cd -
 if [ -f gitauto.sh  ]
 then
 echo '#Git sucessful' >> output.txt
