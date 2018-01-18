49ee15472c3cf8735c7b2220592316eb9307e614
phanirithvij <phanirithvij20003@gmail.com>
2018-01-18 15:10:22 +0530

    MADE MY DAY

--- ex.sh
+++ ex.sh
@@ -6,7 +6,10 @@ echo $i > ex2.sh
 chmod +x ex2.sh
 echo $i >> output.txt
 ./ex2.sh >> output.txt
+	if [[ "$(echo $i | sed '/^$/d;s/[[:blank:]]//g' | head -c 1)" != "#" ]]
+	then
 echo " " >> output.txt
+	fi
 done
 if [ -f gitauto.sh  ]
 then
